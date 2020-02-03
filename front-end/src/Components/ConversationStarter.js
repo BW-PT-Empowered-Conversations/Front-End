42a0a579dde7decd55da640db0019e67ac428729
Elisabeth <elisabethearley@gmail.com>
2020-02-03 18:26:49 -0500

    created UserDashboard.js

--- front-end/src/Components/ConversationStarter.js
+++ front-end/src/Components/ConversationStarter.js
@@ -1,10 +1,1 @@
-import React from 'react';
-
-export default function ConversationStarter() {
-  return (
-      <div>
-        <h1>Welcome</h1>
-        <button>New Convo</button>
-      </div>
-  );
-}
+import react from 'react';
