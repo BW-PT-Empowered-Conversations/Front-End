5859ba65c2b9d5867fba8d5f672e713997c0618d
mandihamza <mandihamza@gmail.com>
2020-02-05 18:41:29 -0700

    Fixes route setup

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 
 const AppWithProvider = (
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+<Provider store={store}>
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>
   </Provider>
 )
 
